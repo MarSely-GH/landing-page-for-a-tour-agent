6380029c0d1d05e92248faac4c5284ce6cee3465
User <user@localhost>
2026-05-10 04:35:57 +0500

    изменил для тг не id а со ссылкой

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -6,7 +6,7 @@ function contactCtaButtons() {
         <span class="micro">Отвечу лично</span>
       </div>
       <div class="cta-item">
-        <a class="btn btn-telegram" href="https://t.me/turagentonline72" target="_blank" rel="noopener noreferrer">Написать в Telegram</a>
+        <a class="btn btn-telegram" href="https://web.telegram.org/a/#471578666" target="_blank" rel="noopener noreferrer">Написать в Telegram</a>
         <span class="micro">Подберу тур под ваш бюджет</span>
       </div>
       <div class="cta-item">
@@ -19,7 +19,7 @@ function contactCtaButtons() {
         <span class="micro">Отвечу лично в VK</span>
       </div>
       <div class="cta-item">
-        <a class="btn btn-light" href="https://t.me/turagentonline72" target="_blank" rel="noopener noreferrer">Получить подборку туров</a>
+        <a class="btn btn-light" href="https://web.telegram.org/a/#471578666" target="_blank" rel="noopener noreferrer">Получить подборку туров</a>
         <span class="micro">Без обязательств</span>
       </div>
     </div>
